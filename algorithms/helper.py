89ab2a0b38c45d3d12cff872f96bc70d9e6ab514
JaumeFigueras <jaume.figueras@upc.edu>
2020-08-06 12:40:06 +0200

    Added comments

--- algorithms/helper.py
+++ algorithms/helper.py
@@ -2,4 +2,13 @@ import numpy as np
 from scipy.spatial import distance as dst
 
 def ComputeDistanceMatrix(points):
+    """Computes a distance matrix from all the points in a list.
+
+    :param points: a list of n-dimensional points
+    :type points: list of lists
+
+    :return: a symetric distance matrix
+    :type return: matrix
+
+    """
     return dst.cdist(points, points, 'euclidean')
